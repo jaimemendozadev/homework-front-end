9759ea5a77f92890f3262fc2e27600f0b4d0b7a7
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-14 11:37:04 -0700

    Add router scaffolding

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import App from "./app/index.jsx";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<BrowserRouter>
+  <Switch>
+    <Route path="/" component={App} />
+  </Switch>
+</BrowserRouter>, document.getElementById("root"));
