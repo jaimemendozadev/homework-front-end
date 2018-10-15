fc127efc1da5cda3ae9135a30e6645abb3ffaa19
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-15 11:35:45 -0700

    Start work on tablet layout

--- src/app/components/GifView/index.jsx
+++ src/app/components/GifView/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import GifPlaceHolder from "../GifPlaceHolder/index.jsx";
-import { getWindowDimensions, setCurrentLayout } from "./utils";
+import { getWindowDimensions, setCurrentLayout } from "../utils";
 
 const defaultState = {
   width: 0,
--- src/app/components/utils.js
+++ src/app/components/utils.js
@@ -1,7 +1,13 @@
+// Possible className layouts: mobile-layout, tablet-layout, desktop-layout
+
 export const setLayoutGifSize = (gif, currentLayout) => {
   // gif.images.fixed_height.url
   if (currentLayout === "mobile-layout") {
     return `${gif.images.fixed_height.url}`;
+  }
+
+  if (currentLayout === "tablet-layout") {
+    return `${gif.images.downsized_medium.url}`;
   }
 
   if (currentLayout === "GifCard") {
@@ -9,4 +15,29 @@ export const setLayoutGifSize = (gif, currentLayout) => {
   }
 
   return "";
+};
+
+export const getWindowDimensions = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  return { width, height };
+};
+
+export const setCurrentLayout = width => {
+  let currentLayout;
+
+  if (width > 1200) {
+    currentLayout = "desktop-layout";
+  }
+
+  if (width >= 481 && width <= 1024) {
+    currentLayout = "tablet-layout";
+  }
+
+  if (width <= 480) {
+    currentLayout = "mobile-layout";
+  }
+
+  return currentLayout;
 };
