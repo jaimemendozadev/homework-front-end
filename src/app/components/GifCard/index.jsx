796c7bae432a0ebcf0d8c8b5d8b43f36cd4df90d
jaimemendozadev <jaimemendozadev@gmail.com>
2018-10-15 10:04:50 -0700

    Render working version of mobile GifCard

--- src/app/components/GifCard/index.jsx
+++ src/app/components/GifCard/index.jsx
@@ -3,8 +3,12 @@ import Logo from "../../assets/dancing-slack-penguin.gif";
 
 const GifCard = props => {
   console.log("props inside GifCard ", props);
-  const { gif } = props.location.state;
-  const { title, username, images } = gif;
+  const {
+    location: {
+      state: { gif }
+    }
+  } = props;
+  const { title, username, images, url } = gif;
 
   console.log("gif inside GifCard ", gif);
 
@@ -12,21 +16,29 @@ const GifCard = props => {
     <div className="app-container">
       <div className="gifcard-container">
         <div className="gifstagram-icon">
-          <img src={`/${Logo}`} />
+          <div className="logo-container">
+            <img src={`/${Logo}`} />
+          </div>
+
+          <h2>Gifstagram</h2>
         </div>
 
         <div className="gifcard-gif-container">
           <img src={images.original.url} />
+
+          {/* <img src={`https://media0.giphy.com/media/xT8qBmPuOVffLrlKVi/giphy.gif`} /> */}
         </div>
 
         <div className="gifcard-text-container">
           <h4>
             Title:
-            {` ${title.toUpperCase()}`}
+            <a href={url} rel="noopener noreferrer" target="_blank">
+              {` ${title.toUpperCase()}`}
+            </a>
           </h4>
+
           <h4>
             Gif By:
-{" "}
             <a
               href={`https://giphy.com/${username}`}
               rel="noopener noreferrer"
@@ -35,6 +47,7 @@ const GifCard = props => {
               @{username}
             </a>
           </h4>
+          
         </div>
       </div>
     </div>
